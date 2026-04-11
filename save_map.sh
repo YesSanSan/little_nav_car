4f7b0d0df3c86d2af5bfd86dfbde821e94c065ae
YanSanwu <ysw351@outlook.com>
2026-04-12 07:43:50 +0800

    修复地图保存

--- save_map.sh
+++ save_map.sh
@@ -1,1 +1,1 @@
-ros2 run nav2_map_server map_saver_cli -f src/lc_navigation/map/my_map
+. /opt/ros/jazzy/setup.bash && . ./install/setup.bash && ros2 run nav2_map_server map_saver_cli -f src/lc_navigation/map/my_map
